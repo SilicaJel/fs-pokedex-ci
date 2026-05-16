f83d0e45196db8325e7c514b7d091b3e1ab19b95
SilicaJel <shahisambridh@gmail.com>
2026-05-16 12:50:00 +0300

    fix: move jest.config.js to root and update jest.setup.js

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './e2e-tests',
+  workers: process.env.CI ? 1 : undefined,
   webServer: {
-    command: 'npm start',
+    command: 'npm run build && npm start',
     url: 'http://127.0.0.1:8080',
     reuseExistingServer: !process.env.CI,
-    timeout: 120000
+    timeout: 300000
   }
 });
